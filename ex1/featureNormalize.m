d30066e34d84da6a5305f2a5ebd5ce74093df633
kawano8811 <y.kawano0723@gmail.com>
2020-06-02 22:40:15 +0900

    fix normalization formula

--- ex1/featureNormalize.m
+++ ex1/featureNormalize.m
@@ -1,5 +1,5 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
-%FEATURENORMALIZE Normalizes the features in X 
+%FEATURENORMALIZE Normalizes the features in X
 %   FEATURENORMALIZE(X) returns a normalized version of X where
 %   the mean value of each feature is 0 and the standard deviation
 %   is 1. This is often a good preprocessing step to do when
@@ -9,18 +9,9 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
-       
+
 mu = mean(X);
 sigma = std(X);
-X_norm(:,1) = (X(:,1) - mu(:, 1)) / sigma(:,1);
-X_norm(:,2) = (X(:,2) - mu(:, 2)) / sigma(:,2);
-
-
-
-
-
-
-
-% ============================================================
+X_norm = (X - mu) ./ sigma;
 
 end
