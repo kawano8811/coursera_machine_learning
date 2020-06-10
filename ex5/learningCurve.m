c23260d94b7263b6188899de01dc0faf3296b09b
Kawano Yoshitaka <y.kawano0723@gmail.com>
2020-06-10 13:20:40 +0900

    implement learningCurve and polyFeatures

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -21,56 +21,17 @@ m = size(X, 1);
 error_train = zeros(m, 1);
 error_val   = zeros(m, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return training errors in 
-%               error_train and the cross validation errors in error_val. 
-%               i.e., error_train(i) and 
-%               error_val(i) should give you the errors
-%               obtained after training on i examples.
-%
-% Note: You should evaluate the training error on the first i training
-%       examples (i.e., X(1:i, :) and y(1:i)).
-%
-%       For the cross-validation error, you should instead evaluate on
-%       the _entire_ cross validation set (Xval and yval).
-%
-% Note: If you are using your cost function (linearRegCostFunction)
-%       to compute the training and cross validation error, you should 
-%       call the function with the lambda argument set to 0. 
-%       Do note that you will still need to use lambda when running
-%       the training to obtain the theta parameters.
-%
-% Hint: You can loop over the examples with the following:
-%
-%       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
-%       end
-%
-
-% ---------------------- Sample Solution ----------------------
-for i = 2:m
+for i = 1:m
   % subset of X, y
   Xi = X(1:i, :);
   yi = y(1:i);
-  Xvali = Xval(1:i, :);
-  yvali = yval(1:i);
+
   % training
-  [theta] = trainLinearReg([ones(i, 1) Xi], yi, lambda);
-  error_train(i) = linearRegCostFunction([ones(i, 1) Xi], yi, theta, lambda);
-  error_val(i) = linearRegCostFunction([ones(i, 1) Xvali], yvali, theta, 0); % lambda=0
+  [theta] = trainLinearReg(Xi, yi, lambda);
+
+  % error
+  error_train(i) = linearRegCostFunction(Xi, yi, theta, 0); % lambda=0
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0); % lambda=0
 endfor
-
-
-
-
-
-
-% -------------------------------------------------------------
-
-% =========================================================================
 
 end
