90cb917a3e4b7cad6751e0ce300ed678c9cdb944
Kawano Yoshitaka <y.kawano0723@gmail.com>
2020-06-15 10:21:05 +0900

    implement estimateGaussian.m

--- ex8/estimateGaussian.m
+++ ex8/estimateGaussian.m
@@ -14,23 +14,7 @@ function [mu sigma2] = estimateGaussian(X)
 mu = zeros(n, 1);
 sigma2 = zeros(n, 1);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the mean of the data and the variances
-%               In particular, mu(i) should contain the mean of
-%               the data for the i-th feature and sigma2(i)
-%               should contain variance of the i-th feature.
-%
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
+mu = (1/m) * sum(X);
+sigma2 = (1/m) * sum(power((X - mu), 2));
 
 end
