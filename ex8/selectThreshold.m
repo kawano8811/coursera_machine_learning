51590b6f2f119113d9ccf13235d6c3f325167a1e
Kawano Yoshitaka <y.kawano0723@gmail.com>
2020-06-15 11:00:38 +0900

    implement selectThreshold.m

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -12,30 +12,24 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Compute the F1 score of choosing epsilon as the
-    %               threshold and place the value in F1. The code at the
-    %               end of the loop will compare the F1 score for this
-    %               choice of epsilon and set it to be the best epsilon if
-    %               it is better than the current choice of epsilon.
-    %               
-    % Note: You can use predictions = (pval < epsilon) to get a binary vector
-    %       of 0's and 1's of the outlier predictions
 
+    predictions = (pval < epsilon);
+    % True Positive, prediction is possitive and groud truth is positive
+    tp = sum(predictions == 1 & yval == 1);
+    % False Positive, prediction is possitive but groud truth is negative
+    fp = sum(predictions == 1 & yval == 0);
+    % False Negative, prediction is negative but groud truth is positive
+    fn = sum(predictions == 0 & yval == 1);
 
+    % precision and recall
+    if ((tp+fp == 0) || (tp+fn == 0))
+      continue
+    end
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
 
-
-
-
-
-
-
-
-
-
-
-    % =============================================================
+    % F1 score
+    F1 = (2 * prec * rec) / (prec + rec);
 
     if F1 > bestF1
        bestF1 = F1;
